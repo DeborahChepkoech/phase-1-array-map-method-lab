76bf158ef2a1566d4f2a52fd9264e265d2099eca
deborahchepkoech <deborah.chepkoech@student.moringaschool.com>
2025-03-18 11:13:00 +0300

    Added mapping

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(function(title) {
+    const words = title.split(' ');
+    const capitalizedWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
